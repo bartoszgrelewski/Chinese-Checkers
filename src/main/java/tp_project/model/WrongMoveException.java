8ee2d303c9dfbb39f88d0d8e0cbe060706ae0c4d
Acer HOME <261700@student.pwr.edu.pl>
2022-01-13 03:54:03 +0100

    Fourth Wiktoria's commit

---
+++ src/main/java/tp_project/model/WrongMoveException.java
@@ -0,0 +1,13 @@
+package tp_project.model;
+
+/**
+ * long number to throw exception
+ */
+public class WrongMoveException extends Exception {
+
+    private static final long serialVersionUID = -38339032451684889L;
+
+    WrongMoveException() {
+        super();
+    }
+}
